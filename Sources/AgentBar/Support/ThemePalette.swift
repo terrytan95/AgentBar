fe75239fce31beefc850667d83c2bdb39bf324bf
Terry Tan <tmy1995hflc@gmail.com>
2026-08-08 23:35:44 -0700

    style(theme): boost palette saturation

--- Sources/AgentBar/Support/ThemePalette.swift
+++ Sources/AgentBar/Support/ThemePalette.swift
@@ -48,15 +48,15 @@ enum AgentBarColorTheme: String, CaseIterable, Identifiable {
 
     private var colors: (light: NSColor, dark: NSColor) {
         switch self {
-        case .classic: (light: NSColor(red: 0.29, green: 0.50, blue: 0.70, alpha: 1), dark: NSColor(red: 0.55, green: 0.72, blue: 0.88, alpha: 1))
-        case .mistSage: (light: NSColor(red: 0.35, green: 0.42, blue: 0.39, alpha: 1), dark: NSColor(red: 0.65, green: 0.71, blue: 0.68, alpha: 1))
-        case .hazeBlue: (light: NSColor(red: 0.36, green: 0.43, blue: 0.49, alpha: 1), dark: NSColor(red: 0.67, green: 0.72, blue: 0.77, alpha: 1))
-        case .dustyRose: (light: NSColor(red: 0.47, green: 0.37, blue: 0.40, alpha: 1), dark: NSColor(red: 0.77, green: 0.68, blue: 0.71, alpha: 1))
-        case .mutedClay: (light: NSColor(red: 0.48, green: 0.38, blue: 0.36, alpha: 1), dark: NSColor(red: 0.78, green: 0.69, blue: 0.67, alpha: 1))
-        case .moss: (light: NSColor(red: 0.38, green: 0.42, blue: 0.32, alpha: 1), dark: NSColor(red: 0.70, green: 0.73, blue: 0.61, alpha: 1))
-        case .smokedLavender: (light: NSColor(red: 0.41, green: 0.38, blue: 0.46, alpha: 1), dark: NSColor(red: 0.72, green: 0.69, blue: 0.76, alpha: 1))
-        case .stoneTaupe: (light: NSColor(red: 0.44, green: 0.40, blue: 0.36, alpha: 1), dark: NSColor(red: 0.73, green: 0.69, blue: 0.64, alpha: 1))
-        case .lakeGray: (light: NSColor(red: 0.35, green: 0.44, blue: 0.44, alpha: 1), dark: NSColor(red: 0.66, green: 0.74, blue: 0.73, alpha: 1))
+        case .classic: (light: NSColor(red: 0.26, green: 0.49, blue: 0.70, alpha: 1), dark: NSColor(red: 0.51, green: 0.70, blue: 0.88, alpha: 1))
+        case .mistSage: (light: NSColor(red: 0.33, green: 0.42, blue: 0.38, alpha: 1), dark: NSColor(red: 0.62, green: 0.71, blue: 0.67, alpha: 1))
+        case .hazeBlue: (light: NSColor(red: 0.34, green: 0.42, blue: 0.49, alpha: 1), dark: NSColor(red: 0.64, green: 0.70, blue: 0.77, alpha: 1))
+        case .dustyRose: (light: NSColor(red: 0.47, green: 0.35, blue: 0.39, alpha: 1), dark: NSColor(red: 0.77, green: 0.65, blue: 0.69, alpha: 1))
+        case .mutedClay: (light: NSColor(red: 0.48, green: 0.36, blue: 0.34, alpha: 1), dark: NSColor(red: 0.78, green: 0.66, blue: 0.64, alpha: 1))
+        case .moss: (light: NSColor(red: 0.37, green: 0.42, blue: 0.30, alpha: 1), dark: NSColor(red: 0.69, green: 0.73, blue: 0.58, alpha: 1))
+        case .smokedLavender: (light: NSColor(red: 0.40, green: 0.36, blue: 0.46, alpha: 1), dark: NSColor(red: 0.70, green: 0.66, blue: 0.76, alpha: 1))
+        case .stoneTaupe: (light: NSColor(red: 0.44, green: 0.39, blue: 0.34, alpha: 1), dark: NSColor(red: 0.73, green: 0.68, blue: 0.61, alpha: 1))
+        case .lakeGray: (light: NSColor(red: 0.33, green: 0.44, blue: 0.44, alpha: 1), dark: NSColor(red: 0.63, green: 0.74, blue: 0.73, alpha: 1))
         }
     }
 

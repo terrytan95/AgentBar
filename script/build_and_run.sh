15c470937d80e117ce602285e292850f9a8efae7
Terry Tan <tmy1995hflc@gmail.com>
2026-08-21 03:16:57 -0700

    chore(release): bump to v2.6.8
    Bumps AgentBar to v2.6.8 (build 256) for the auto access token refresh setting.

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -9,8 +9,8 @@ else
   BUNDLE_ID="com.terrytan.AgentBarPreview"
 fi
 MIN_SYSTEM_VERSION="14.0"
-APP_VERSION="2.6.7"
-APP_BUILD="255"
+APP_VERSION="2.6.8"
+APP_BUILD="256"
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 DIST_DIR="$ROOT_DIR/dist"
